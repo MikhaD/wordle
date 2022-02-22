234e9a14a942424e40e626a4012e9765630df4f5
Mikha Davids <mikhadavids@gmail.com>
2022-02-22 14:22:23 +0200

    fix: :bug: messy transition between timer & refresh button

--- src/components/widgets/Timer.svelte
+++ src/components/widgets/Timer.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher, onDestroy } from "svelte";
 
-	import { blur } from "svelte/transition";
 	import type { GameMode } from "../../enums";
 	import { mode } from "../../stores";
 	import { modeData } from "../../utils";
@@ -33,13 +32,13 @@
 <h3>Next wordle</h3>
 <div class="container">
 	{#if ms > 0}
-		<div transition:blur class="timer">
+		<div class="timer">
 			{`${Math.floor(ms / HOUR)}`.padStart(2, "0")}:{`${Math.floor(
 				(ms % HOUR) / MINUTE
 			)}`.padStart(2, "0")}:{`${Math.floor((ms % MINUTE) / SECOND)}`.padStart(2, "0")}
 		</div>
 	{:else}
-		<div transition:blur class="button" on:click={() => dispatch("reload")}>
+		<div class="button" on:click={() => dispatch("reload")}>
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
 				<path
 					d="M4.609 12c0-4.082 3.309-7.391 7.391-7.391a7.39 7.39 0 0 1 6.523 3.912l-1.653 1.567H22v-5.13l-1.572 1.659C18.652 3.841 15.542 2 12 2 6.477 2 2 6.477 2 12s4.477 10 10 10c4.589 0 8.453-3.09 9.631-7.301l-2.512-.703c-.871 3.113-3.73 5.395-7.119 5.395-4.082 0-7.391-3.309-7.391-7.391z"
