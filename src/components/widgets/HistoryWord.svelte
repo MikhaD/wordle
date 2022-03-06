bf44a308d9d551e057a0ca4e12545527e8df0ab9
rbrignall <rbrignall@gmail.com>
2022-03-06 18:50:51 +0000

    Fledgling history widget

---
+++ src/components/widgets/HistoryWord.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { mode } from "../../stores";
+	import { modeData } from "../../utils";
+
+	export let word: string;
+
+</script>
+
+<h3>{word}</h3>
+<div>
+TODO
+</div>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		gap: 8px;
+	}
+</style>
