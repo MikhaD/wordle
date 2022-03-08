57bead30b07729746db508c895be80c0a7680e5a
rbrignall <rbrignall@gmail.com>
2022-03-08 23:13:40 +0000

    Update index.ts

--- src/components/widgets/index.ts
+++ src/components/widgets/index.ts
@@ -3,7 +3,6 @@ export { default as Share } from "./Share.svelte";
 export { default as Tutorial } from "./Tutorial.svelte";
 export { default as Timer } from "./Timer.svelte";
 export { default as Toaster } from "./Toaster.svelte";
-export { default as Tooltip } from "./Tooltip.svelte";
 export { default as ShareGame } from "./ShareGame.svelte";
 export { default as HistoryWord } from "./HistoryWord.svelte";
 export * from "./stats";
