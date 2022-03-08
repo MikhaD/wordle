59592f57fba0084c2984500d03c2a96703c9ce99
rbrignall <rbrignall@gmail.com>
2022-03-08 23:12:55 +0000

    Tooltip for gameicons

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,6 @@
 	import { modeData, getWordNumber } from "../utils";
 	import GameIcon from "./GameIcon.svelte";
 	import type { Toaster } from "./widgets";
-
 	export let showStats: boolean;
 	export let showRefresh: boolean;
     export let gameNumber: number;
@@ -35,14 +34,14 @@
 				/>
 			</GameIcon>
         {:else}
-			<GameIcon onClick={() => $mode = ($mode + 1) % modeData.modes.length}> <!-- clock -->
+			 <GameIcon onClick={() => $mode = ($mode + 1) % modeData.modes.length} Tooltip="Play historical games"> <!-- clock -->
                 <path class:hist={modeData.modes[$mode].historical}
                     d="M12.9 5.5h-1.8v7.2l6.24 3.84.96-1.56-5.4-3.24zM12 2C6.48 2 2 6.48 2 12S6.48 22 12 22 22 17.52 22 12 17.52 2 12 2zM12 20c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8S16.41 20 12 20z"
                 />
-			</GameIcon>
+			 </GameIcon>
         {/if}
         {#if modeData.modes[$mode].historical && gameNumber > 0} <!-- left arrow -->
-			<GameIcon onClick={() => dispatch("prevhistgame")}>
+			<GameIcon onClick={() => dispatch("prevhistgame")} Tooltip="Game #{gameNumber}">
                 <path 
                     transition:fade={{ duration: 200 }}  
                     d="m4.431 12.822l13 9A1 1 0 0 0 19 21V3a1 1 0 0 0-1.569-.823l-13 9a1.003 1.003 0 0 0 0 1.645z"
@@ -50,15 +49,17 @@
 			</GameIcon>
 		{/if}        
 	</div>
-	<h1>
-		byrdle
-        {#if modeData.modes[$mode].historical}
-            {gameNumber + 1}
-        {/if}
-	</h1>
-	<div class="icons">
+    <h1>
+    {#if modeData.modes[$mode].historical}
+            #{gameNumber + 1}
+    {:else}
+		  byrdle
+    {/if}
+	   </h1>
+
+    <div class="icons">
         {#if modeData.modes[$mode].historical && gameNumber < getWordNumber() - 1}
-			<GameIcon onClick={() => dispatch("nexthistgame")}> <!-- right arrow -->
+			<GameIcon onClick={() => dispatch("nexthistgame")} Tooltip="Game #{gameNumber+2}"> <!-- right arrow -->
                 <path 
                     transition:fade={{ duration: 200 }}  
                     d="M5.536 21.886a1.004 1.004 0 0 0 1.033-.064l13-9a1 1 0 0 0 0-1.644l-13-9A1 1 0 0 0 5 3v18a1 1 0 0 0 .536.886z"
@@ -73,7 +74,7 @@
 				/>
 			</GameIcon>
 		{/if}
-		<GameIcon onClick={() => dispatch("settings")}>
+		<GameIcon onClick={() => dispatch("settings")} Tooltip="settings">
 			<path
 				d="M19.14,12.94c0.04-0.3,0.06-0.61,0.06-0.94c0-0.32-0.02-0.64-0.07-0.94l2.03-1.58c0.18-0.14,0.23-0.41,0.12-0.61 l-1.92-3.32c-0.12-0.22-0.37-0.29-0.59-0.22l-2.39,0.96c-0.5-0.38-1.03-0.7-1.62-0.94L14.4,2.81c-0.04-0.24-0.24-0.41-0.48-0.41 h-3.84c-0.24,0-0.43,0.17-0.47,0.41L9.25,5.35C8.66,5.59,8.12,5.92,7.63,6.29L5.24,5.33c-0.22-0.08-0.47,0-0.59,0.22L2.74,8.87 C2.62,9.08,2.66,9.34,2.86,9.48l2.03,1.58C4.84,11.36,4.8,11.69,4.8,12s0.02,0.64,0.07,0.94l-2.03,1.58 c-0.18,0.14-0.23,0.41-0.12,0.61l1.92,3.32c0.12,0.22,0.37,0.29,0.59,0.22l2.39-0.96c0.5,0.38,1.03,0.7,1.62,0.94l0.36,2.54 c0.05,0.24,0.24,0.41,0.48,0.41h3.84c0.24,0,0.44-0.17,0.47-0.41l0.36-2.54c0.59-0.24,1.13-0.56,1.62-0.94l2.39,0.96 c0.22,0.08,0.47,0,0.59-0.22l1.92-3.32c0.12-0.22,0.07-0.47-0.12-0.61L19.14,12.94z M12,15.6c-1.98,0-3.6-1.62-3.6-3.6 s1.62-3.6,3.6-3.6s3.6,1.62,3.6,3.6S13.98,15.6,12,15.6z"
 			/>
