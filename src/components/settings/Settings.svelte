954dbbaf552d7353f85316e70fe5b0805758c07f
rbrignall <rbrignall@gmail.com>
2022-05-25 22:52:18 +0100

    v5.4.1

--- src/components/settings/Settings.svelte
+++ src/components/settings/Settings.svelte
@@ -106,7 +106,7 @@
         </div>
 	</div>
     <div class="footer">
-        <div>Version: Byrd 5.4.0</div>
+        <div>Version: Byrd 5.4.1</div>
         <div class="word">
             Game #{wordNumber+1}
         </div>
