9e37ffbb51cb7c7d5442e9a3f5c15d867283cd21
Mikha Davids <mikhadavids@gmail.com>
2023-01-08 13:33:37 +0200

    fix: :ambulance: added base url to config to prevent CORS issue

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/wordle/",
   plugins: [svelte({
     preprocess: vitePreprocess()
   })],
