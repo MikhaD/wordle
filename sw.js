c3e7ac12718a9de8deae9b40915d61be691a5868
MikhaD <MikhaD@users.noreply.github.com>
2023-01-14 22:03:09 +0000

    deploy: 18631175dcb58fc826f55ce2f32df4b0d7cbe66b

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const version = "1.5.0";
+const version = "1.5.2";
 const cacheName = `wordle+-v${version}`;
 
 const assetsToCache = [
