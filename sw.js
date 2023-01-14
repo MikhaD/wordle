c2c3795bde2edec9356ab13e0221ff614c005b6e
MikhaD <MikhaD@users.noreply.github.com>
2023-01-14 13:07:27 +0000

    deploy: 40385dfd15977b923b4fcecfe52be5718516a15d

--- sw.js
+++ sw.js
@@ -1,14 +1,23 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const cacheName = "wordle+-v1.4.0";
+const version = "1.5.0";
+const cacheName = `wordle+-v${version}`;
 
 const assetsToCache = [
 	"./",
 	"./index.html",
+	`./index-${version}.css`,
+	`./index-${version}.js`,
 	"./global.css",
-	"./build/bundle.css",
-	"./build/bundle.js"
 ];
+
+/**
+ * Special log function to clearly distinguish logs from the service worker.
+ * @param {string} text - The text to log
+ */
+function log(text) {
+	console.log("%cService Worker", "color: purple; font-weight: 600; background: white; padding: 0 5px; border-radius: 2px", text);
+}
 
 self.addEventListener("install", e => {
 	log("Installing");
@@ -33,6 +42,7 @@ self.addEventListener("fetch", e => {
 		return response;
 	})());
 });
+
 // When the name of the cache changes (because the app has been updated) delete the old cache
 self.addEventListener("activate", e => {
 	e.waitUntil(caches.keys().then(keys => {
@@ -43,10 +53,3 @@ self.addEventListener("activate", e => {
 		}));
 	}));
 });
-/**
- * Special log function to clearly distinguish logs from the service worker.
- * @param {string} text - The text to log
- */
-function log(text) {
-	console.log("%cService Worker", "color: purple; font-weight: 600; background: white; padding: 0 5px; border-radius: 2px", text);
-}
