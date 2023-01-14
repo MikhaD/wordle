40385dfd15977b923b4fcecfe52be5718516a15d
Mikha Davids <mikhadavids@gmail.com>
2023-01-14 15:06:04 +0200

    chore: :bookmark: changed version to 1.5.0

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 //! IF ANYTHING IN THIS FILE IS CHANGED MAKE SURE setVersion.js HAS ALSO BEEN UPDATED
 // Increase the v number when the app is updated
-const version = "1.4.0";
+const version = "1.5.0";
 const cacheName = `wordle+-v${version}`;
 
 const assetsToCache = [
